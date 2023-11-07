1e0693c7f935849b8191d98b9c65cfe2104363b1
DevFlikr <147554723+devflikr@users.noreply.github.com>
2023-11-07 09:55:34 +0530

    Patch v1.4

--- src/util/cookies.ts
+++ src/util/cookies.ts
@@ -3,10 +3,11 @@ import { ExpressResponse } from "../types/Express";
 
 export default function setResponseCookies(res: ExpressResponse, name: string, value: string | number | null, config: CookieOptions = {}) {
     return res.cookie(name, value, {
-        domain: ".netlify.app",
+        // domain: ".netlify.app",
         expires: (value == null ? new Date(Date.now()) : new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)),
-        sameSite: "none",
-        path: "/",
+        sameSite: process.env.MODE === "development" ? "lax" : "none",
+        secure: process.env.NODE_ENV === "development" ? false : true,
+        httpOnly: true,
         ...config,
     });
 }
