620b1f712790605401a7df19317e54d4da7eb2a2
DevFlikr <147554723+devflikr@users.noreply.github.com>
2023-11-07 22:28:51 +0530

    Patch v1.11

--- src/util/cookies.ts
+++ src/util/cookies.ts
@@ -6,6 +6,8 @@ export default function setResponseCookies(res: ExpressResponse, name: string, v
         domain: ".devflikr.com",
         expires: (value == null ? new Date(Date.now()) : new Date(Date.now() + 365 * 24 * 60 * 60 * 1000)),
         sameSite: process.env.MODE === "development" ? "none" : "lax",
+        httpOnly: process.env.MODE === "development",
+        secure: process.env.MODE === "development",
         ...config,
     });
 }
